1acb88e0daaf7aa54c96aefd7edbdf2473d8080e
PutuRama <pratamaputurama@gmail.com>
2022-07-02 14:53:56 +0800

    typing-effect v0

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const tagline = ['Educate' + " Children" + ' Making' + ' Useful' + ' for' + ' State', ''];
+let gone = 0;
+let numbering = 0;
+let CurrentItem = '';
+let tag = '';
+
+
+//testing
+// document.querySelector('.typing-effect').innerHTML = length;
+pop();
+function pop() {
+  if (gone == tagline.length) {
+  gone = 0;
+}
+
+  CurrentItem = tagline[gone];
+  tag = CurrentItem.slice(0, ++numbering);
+
+  console.log(tag);
+  document.querySelector('.typing-effect').textContent = tag;
+
+  if (tag.length == CurrentItem.length) {
+    gone++;
+    numbering = 0;
+  }
+
+  setTimeout(pop, 400);
+};
+
